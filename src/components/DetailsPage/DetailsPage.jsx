f30e44d6a2e688f821cbf8ec966e561cdb998822
JuiceWRLD <999alishafiei999@gmail.com>
2023-08-06 17:24:16 +0200

    :)

--- src/components/DetailsPage/DetailsPage.jsx
+++ src/components/DetailsPage/DetailsPage.jsx
@@ -1,8 +1,16 @@
 import {
   Box,
+  Button,
   CircularProgress,
   Container,
+  Divider,
+  FormControl,
   Grid,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Rating,
+  Select,
   Typography,
 } from "@mui/material";
 import React, { useEffect, useState } from "react";
@@ -11,6 +19,12 @@ import Header from "../Header/Header";
 
 const DetailsPage = () => {
   const [products, setProducts] = useState(null);
+
+  const [amount, setAmount] = React.useState(1);
+
+  const handleChange = (event) => {
+    setAmount(event.target.value);
+  };
 
   const { id } = useParams();
 
@@ -22,14 +36,148 @@ const DetailsPage = () => {
 
   return (
     <Container maxWidth="xl">
-      <Box sx={{ display: "flex", marginTop: "90px" }}>
+      <Box sx={{ display: "flex", marginTop: "90px", padding: "24px" }}>
         <Header />
         {products ? (
-          <Grid key={id} item xs={6} sm={4} md={2}>
-            <Typography>{products.title}</Typography>
+          <Grid container>
+            <Grid container item xs={9}>
+              <Grid item xs={6}>
+                <Paper variant="outlined" square>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                    }}
+                  >
+                    <img
+                      src={products.image}
+                      style={{
+                        width: "461px",
+                        height: "461px",
+                        objectFit: "contain",
+                        padding: 20,
+                      }}
+                    />
+                  </Box>
+                </Paper>
+              </Grid>
+              <Grid item sx={{ p: 2 }} xs={6}>
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                  <Typography variant="h6">{products.title}</Typography>
+                </Box>
+                <Divider sx={{ marginY: "20px" }} />
+                <Box
+                  sx={{
+                    display: "flex",
+                    gap: 0.5,
+                    justifyContent: "flex-start",
+                  }}
+                >
+                  <Typography>{products.rating.rate}</Typography>
+                  <Rating
+                    name="read-only"
+                    value={products.rating.rate}
+                    readOnly
+                    precision={0.5}
+                  />
+
+                  <Typography textTransform="none" variant="button">
+                    {products.rating.count} Reviews
+                  </Typography>
+                </Box>
+                <Divider sx={{ marginY: "20px" }} />
+                <Box sx={{ display: "flex", justifyContent: "flex-start" }}>
+                  <Typography variant="body1">
+                    Price: ${products.price}
+                  </Typography>
+                </Box>
+                <Divider sx={{ marginY: "20px" }} />
+                <Box>
+                  <Typography variant="body1">About this item:</Typography>
+                  <Typography sx={{ marginTop: "5px" }} variant="body2">
+                    {products.description}
+                  </Typography>
+                </Box>
+              </Grid>
+            </Grid>
+            <Grid
+              item
+              xs={3}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <Box>
+                <Paper
+                  sx={{ width: "250px", height: "240px", padding: "20px" }}
+                  variant="outlined"
+                  square
+                >
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Typography variant="body2">Price:</Typography>
+                    <Typography variant="body2">${products.price}</Typography>
+                  </Box>
+                  <Divider sx={{ marginY: "10px" }} />
+                  <Box
+                    sx={{ display: "flex", justifyContent: "space-between" }}
+                  >
+                    <Typography variant="body2">Status:</Typography>
+                    <Typography variant="body2">Available</Typography>
+                  </Box>
+                  <Divider sx={{ marginY: "10px" }} />
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Typography>Amount</Typography>
+                    <FormControl
+                      variant="outlined"
+                      sx={{ paddingBottom: "2px" }}
+                    >
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        placeholder="Amount"
+                        value={amount}
+                        onChange={handleChange}
+                        size="small"
+                        sx={{ width: "80px" }}
+                      >
+                        <MenuItem value={1}>1</MenuItem>
+                        <MenuItem value={2}>2</MenuItem>
+                        <MenuItem value={3}>3</MenuItem>
+                        <MenuItem value={4}>4</MenuItem>
+                        <MenuItem value={5}>5</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                  <Divider sx={{ marginY: "10px" }} />
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "center",
+                      paddingTop: "10px",
+                    }}
+                  >
+                    <Button variant="contained" fullWidth>
+                      Add to Cart
+                    </Button>
+                  </Box>
+                </Paper>
+              </Box>
+            </Grid>
           </Grid>
         ) : (
-          <Box sx={{ width: "100%" }}>
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
             <CircularProgress color="inherit" />
           </Box>
         )}
--- src/components/Main/MainProducts.jsx
+++ src/components/Main/MainProducts.jsx
@@ -19,7 +19,11 @@ const MainProducts = () => {
         {products ? (
           products.map((product) => (
             <Grid key={product.title} item xs={6} sm={4} md={2}>
-              <Link key={product.title} to={`/products/${product.id}`}>
+              <Link
+                style={{ textDecoration: "none" }}
+                key={product.title}
+                to={`/products/${product.id}`}
+              >
                 <MainCard
                   image={product.image}
                   title={product.title}
