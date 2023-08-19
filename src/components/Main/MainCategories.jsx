625548b55e021b5cc50c06b5e155745a1d2226f6
JuiceWRLD <999alishafiei999@gmail.com>
2023-08-19 12:40:18 +0200

    pics for vercel development

--- src/components/Main/MainCategories.jsx
+++ src/components/Main/MainCategories.jsx
@@ -62,21 +62,25 @@ const categories = [
   {
     title: "Electronic Devices",
     image:
-      "../../../images/silver-smartphone-front-side-white-background_187299-20187.jpg",
+      // "../../../images/silver-smartphone-front-side-white-background_187299-20187.jpg",
+      "https://img.freepik.com/premium-vector/mobile-smartphone-phone-mockup-isolated-white-background-with-blank-screen_88272-1105.jpg?size=626&ext=jpg",
   },
   {
     title: "Health and Beauty",
     image:
-      "../../../images/beauty-model-girl-face-portrait-beautiful-woman-touching-face-enjoy-perfect-skin_394555-801.jpg",
+      // "../../../images/beauty-model-girl-face-portrait-beautiful-woman-touching-face-enjoy-perfect-skin_394555-801.jpg",
+      "https://img.freepik.com/premium-photo/beauty-model-girl-face-portrait-beautiful-woman-touching-face-enjoy-perfect-skin_394555-801.jpg?size=626&ext=jpg",
   },
   {
     title: "Fashion and Style",
     image:
-      "../../../images/excited-white-girl-bright-stylish-glasses-posing-pink-dreamy-curly-woman-playing-with-her-ginger-hair-laughing.jpg",
+      // "../../../images/excited-white-girl-bright-stylish-glasses-posing-pink-dreamy-curly-woman-playing-with-her-ginger-hair-laughing.jpg",
+      "https://img.freepik.com/premium-photo/young-stylish-woman-outfit-style-urban-minimalism-illustration-ai-generative_115919-19176.jpg?size=626&ext=jpg",
   },
   {
     title: "Home Furniture",
     image:
-      "../../../images/front-view-armchair-plant-3d-rendering_252032-366-removebg-preview.png",
+      // "../../../images/front-view-armchair-plant-3d-rendering_252032-366-removebg-preview.png",
+      "https://img.freepik.com/premium-psd/front-view-armchair-plant-3d-rendering_252032-366.jpg?size=626&ext=jpg",
   },
 ];
